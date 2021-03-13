4b177b6ee16114ded43c42fcd7e517a498eb4465
Michael Brauninger <mbrauninger@umass.edu>
2021-03-13 13:04:14 -0500

    Initial Commit

---
+++ TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by Michael Brauninger on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
