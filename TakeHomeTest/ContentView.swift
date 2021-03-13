b41668d740065bd22d89d97eb2856c3970d71431
Michael Brauninger <mbrauninger@umass.edu>
2021-03-13 13:18:44 -0500

    Initial

--- TakeHomeTest/ContentView.swift
+++ TakeHomeTest/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+import SwiftUI
+
+/*
+ 
+ Take home coding assignment - Michael Brauninger
+ 
+ Sources:
+ 
+ Setting up shapes: https://www.hackingwithswift.com/quick-start/swiftui/swiftuis-built-in-shapes
+ 
+ Allignment of elements: https://www.hackingwithswift.com/quick-start/swiftui/how-to-force-views-to-one-side-inside-a-stack-using-spacer
+ 
+ Setting up button and counter: https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-tappable-button
+
+ */
+
 struct ContentView: View {
+    
+    @State public var count = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 75, height: 75)
+            Text("Count: \(count)")
+            Spacer()
+            Button {
+                count += 1
+            }
+                label: {
+                    Circle()
+                        .fill(Color.green)
+                        .frame(width: 50)
+                }
+        }
+        .padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+        }
     }
 }
